7bf88a452646bddc483e9018a2e493e68eaea757
Lee Crossley <leee@hotmail.co.uk>
2014-12-02 08:50:33 +0000

    Implement defer

--- mumford.js
+++ mumford.js
@@ -1,17 +1,40 @@
 var when = function (condition) {
     var when = this;
 
-    var iterator = function (then) {
+    var Defer = function () {
+        var deferred;
+
+        var defer = function (callback) {
+            deferred = callback;
+        };
+
+        defer.resolve = function() {
+            if (deferred) {
+                deferred();
+            }
+        };
+
+        return defer;
+    };
+
+    var iterator = function (callback, defer) {
         if (condition()) {
-            then();
+            callback();
+            defer.resolve();
         } else {
-            setTimeout(iterator.bind(this, then), 50);
+            setTimeout(iterator.bind(this, callback, defer), 50);
         }
         return when;
     };
 
+    var then = function (callback) {
+        var defer = new Defer(this);
+        iterator(callback, defer);
+        return defer;
+    };
+
     return {
-        then: iterator
+        then: then
     };
 };
 
